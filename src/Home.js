6170c2cf858bf6dcfa3cf1572c4a4537bef55037
Robert Leedy <leedyrj@gmail.com>
2018-07-30 11:35:31 -0500

    added bloomer.  Navbar is done for now.  need to add working links and styling

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react"
-import Navbar from "./Nav/Navbar";
+import { Container, Box } from 'bloomer'
+import Navigation from "./Nav/Navbar";
 
 export default class Main extends Component {
     render() {
         return (
             <React.Fragment>
-                <Navbar />
-                <div>
-                    <h1>Hello! Welcome to my React App</h1>
-                    <h3>Eventually, this will be really cool and let you do things!</h3>
-                </div>
+                <Navigation />
+                <Container>
+                    <Box>Hello World!</Box>
+                </Container>
             </React.Fragment>
         )
     }
